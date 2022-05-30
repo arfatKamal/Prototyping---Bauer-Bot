ce8d455eaf94f987310f2c860dee4f75ce59fb62
sahat-fahim <sahat-al-ferdous.fahim@stud.hshl.de>
2022-05-30 16:21:14 +0200

    line

--- Line_follower_freeRTOS+counter/src/main.cpp
+++ Line_follower_freeRTOS+counter/src/main.cpp
@@ -2,31 +2,58 @@
 #include <Arduino_FreeRTOS.h>
 
 /*IR sensors */
-const byte IR1_pin = A0;   // Left
-const byte IR2_pin = A1;   // Right
-byte IR1, IR2;
+const uint8_t IR1_pin = A0;   // Left
+const uint8_t IR2_pin = A1;   // Right
+uint8_t IR1, IR2;
 
 /* distance */
-const byte trigPin = 12;
-const byte echoPin = 13;
-int currentDistance;
+const uint8_t trigPin = 12;
+const uint8_t echoPin = 13;
+uint16_t currentDistance;
+
+/* RGB color sensor declaration */
+const uint8_t S0 = 0;
+const uint8_t S1 = 1;
+const uint8_t S2 = 2;
+const uint8_t S3 = 3;
+const uint8_t sensorOut = 4;
+
+uint8_t targetX = 3;
+uint8_t currentX= 0;
+uint8_t y= 1;
+
+volatile uint8_t colorState;  
+const uint8_t cyanState = 0;
+const uint8_t orangeState = 1;
+
+uint8_t redPW = 0;
+uint8_t greenPW = 0;
+uint8_t bluePW = 0;
+
+
+uint8_t redMin = 0; // Red minimum value
+uint8_t redMax= 0; // Red maximum value
+uint8_t greenMin = 0; // Green minimum value
+uint8_t greenMax = 0; // Green maximum value
+uint8_t blueMin = 0; // Blue minimum value
+uint8_t blueMax = 0; // Blue maximum value
 
 /* Motors */
-const byte rightMotor = 10;   // Motor Right connections
-const byte in1 = 9;           // right motor ; for foward LOW
-const byte in2 = 8;           // right motor ; for forward HIGH
+const uint8_t rightMotor = 10;   // Motor Right connections
+const uint8_t in1 = 9;           // right motor ; for foward LOW
+const uint8_t in2 = 8;           // right motor ; for forward HIGH
 
-const byte leftMotor = 5;   // Motor Left connections
-const byte in3 = 7;         // left motor ; for foward LOW
-const byte in4 = 6;         // left motor ; for forward HIGH
+const uint8_t leftMotor = 5;   // Motor Left connections
+const uint8_t in3 = 7;         // left motor ; for foward LOW
+const uint8_t in4 = 6;         // left motor ; for forward HIGH
 
-byte Speed = 80;
+uint8_t Speed = 80;
 
-byte error = 22;
-byte leftSpeed  = Speed+error;
-byte rightSpeed = Speed;
+uint8_t error = 22;
+uint8_t leftSpeed = Speed+error;
+uint8_t rightSpeed = Speed;
 
-byte turnSpeed=10;;
+uint8_t turnSpeed = 10;
 
 
 
@@ -64,11 +91,21 @@ void setup()
   pinMode(IR1_pin, INPUT);
   pinMode(IR2_pin, INPUT);
 
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+
+  pinMode(sensorOut, INPUT);
+
+  digitalWrite(S0,HIGH);
+  digitalWrite(S1,LOW);
+
  
   xTaskCreate(readings, "Sensor readings", configMINIMAL_STACK_SIZE, NULL, 1, &Data);
   xTaskCreate(running, "Car running", configMINIMAL_STACK_SIZE, NULL,1 , &go);
   xTaskCreate(setSpeed, "Incresing Speed", configMINIMAL_STACK_SIZE, NULL,1 , &mSpeed);
-  xTaskCreate(positionCount, "Incresing Speed", configMINIMAL_STACK_SIZE+16, NULL,1 , &axis);
+  xTaskCreate(positionCount, "Counting position", configMINIMAL_STACK_SIZE+16, NULL,1 , &axis);
 
   vTaskStartScheduler();
 }
@@ -137,10 +174,15 @@ void readings(void *argc)
    IR1 = digitalRead(IR1_pin);
    IR2 = digitalRead(IR2_pin);
   
-   Serial.print("Left IR: ");
+   /*Serial.print("Left IR: ");
    Serial.println(IR1);
    Serial.print("Right IR: ");
-   Serial.println(IR2);
+   Serial.println(IR2);*/
+
+  redPW = getRedPW();
+  greenPW = getGreenPW(); 
+  bluePW = getBluePW();
+  
   }
 }
 
@@ -152,9 +194,46 @@ void positionCount(void *argc){
   while (1)
   {
 
+  
+    switch (colorState)
+    {
+    case cyanState:
 
+      if ((redPW >= 4 && redPW <= 5) && (greenPW >= 7  && greenPW <= 9) && ( bluePW >= 6 && bluePW <= 10))
+        {      
+            colorState = orangeState;      
+        }
 
+      break;
+    case orangeState:
+
+     if ((redPW >= 7 && redPW <= 8) && (greenPW >= 3  && greenPW <= 6) && ( bluePW >= 2 && bluePW <= 6))
+        {
+            currentX += 1;
+            colorState = cyanState;      
+        }
+
+      
+      break;
     
+    default:
+      break;
+    }
+
+    uint8_t destination = (targetX - currentX); 
+
+    Serial.print(" Destination : ");
+    Serial.println(destination);
+
+    if (destination == 0)
+    {
+      vTaskSuspend(go);
+      stop(); 
+    }
+    
+
+
+
 
   }
 }
@@ -252,4 +331,50 @@ void distance()
   Serial.print("Distance:");
   Serial.print(currentDistance);
   Serial.println("cm");
+}
+
+
+
+int getRedPW() {
+ 
+  // Set sensor to read Red only
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,LOW);
+  // Define integer to represent Pulse Width
+  int PW;
+  // Read the output Pulse Width
+  PW = pulseIn(sensorOut, LOW);
+  // Return the value
+  return PW;
+ 
+}
+ 
+// Function to read Green Pulse Widths
+int getGreenPW() {
+ 
+  // Set sensor to read Green only
+  digitalWrite(S2,HIGH);
+  digitalWrite(S3,HIGH);
+  // Define integer to represent Pulse Width
+  int PW;
+  // Read the output Pulse Width
+  PW = pulseIn(sensorOut, LOW);
+  // Return the value
+  return PW;
+ 
+}
+ 
+// Function to read Blue Pulse Widths
+int getBluePW() {
+ 
+  // Set sensor to read Blue only
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,HIGH);
+  // Define integer to represent Pulse Width
+  int PW;
+  // Read the output Pulse Width
+  PW = pulseIn(sensorOut, LOW);
+  // Return the value
+  return PW;
+ 
 }
